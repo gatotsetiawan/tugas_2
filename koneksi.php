52d694635863ef7a21816791fb21757368e719c8
gatotsetiawan <48471691+gatotsetiawan@users.noreply.github.com>
2019-03-20 12:33:54 +0700

    Add files via upload

---
+++ koneksi.php
@@ -0,0 +1,7 @@
+<?php
+    
+    $kon = mysqli_connect("localhost","id8951292_akademik","gatot@123","id8951292_akademik");
+    if (mysqli_connect_errno()) {
+        echo "Koneksi database gagal : " .mysqli_connect_error();
+    }
+?>
